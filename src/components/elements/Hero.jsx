4a3de16e9452379881104f7d3c6f9e984fd00400
wahab630 <wahabmalik630@gmail.com>
2022-12-07 18:27:05 +0500

    added

--- src/components/elements/Hero.jsx
+++ src/components/elements/Hero.jsx
@@ -1,69 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import carddata from "../utils/Carddetail.json"
 
-const data = [
-  {
-    img: "assets/one.jpg",
-    name:"Card 1"
-  },
-  {
-    img: "assets/two.jpg",
-    name:"Card 1"
-  },
-  {
-    img: "assets/three.jpg",
-    name:"Card 1"
-  },
-  {
-    img: "assets/four.jpg",
-    name:"Card 1"
-  },
-  {
-    img: "assets/five.jpg",
-    name:"Card 1"
-  }
-];
 
 const Hero = () => {
-  function selectCard() {
-    return (document.getElementById("pick-card").innerHTML =
-      Math.floor(Math.random() * 5) + 1);
-  }
+  const [data, setData] = useState("");
+  const [image, setImage] = useState("");
+  const [title, setTitle]= useState("");
+
+  const myFunction = () => {
+    const random_data = carddata[Math.floor(Math.random() * carddata.length)];
+    console.log(random_data)
+    setData(random_data.name);
+    setImage(random_data.img);
+    setTitle(random_data.title);
+  };
+
+  // const [number,setNumber]=useState("");
+
+  // function selectCard() {
+  //    let rand= Math.floor(Math.random * data.length);
+  // }
   return (
     <>
       <div className="hero">
         <div className="container">
           <div className="row mt-5">
-            {data.map((v, i) => {
-              return (
-                <>
-                  <div className="col-md-4 mb-3 mx-auto" key={0}>
-                    <div className="card bg-dark text-black border-0">
-                      <img src={v.img} className="card-img" alt="no" />
-                      <div className="card-img-overlay ">
-                        <div className=" d-flex align-items-center ">
-                          <h1 className="card-h">{v.name}</h1>
-                        </div>
-                      </div>
+            {
+              carddata.map((item)=>(
+                <div className="col-md-4 mb-3 mx-auto" >
+                <div className="card bg-dark text-black border-0">
+                  <img src={item.img} className="card-img" alt="no" />
+                  <div className="card-img-overlay ">
+                    <div className=" d-flex align-items-center ">
+                      <h1 className="card-h">{item.name}</h1>
                     </div>
                   </div>
-                </>
-              );
-            })}
+                </div>
+              </div>
+              )
+              
+              )
+            }
           </div>
         </div>
         <div className="container">
           <div className="row">
             <div className="col text-center">
-              <button className="btn " onClick={selectCard}>Click Me</button>
+              <button className="btn " onClick={myFunction}>
+                Click Me
+              </button>
             </div>
           </div>
           <div className="row">
             <div className="col-md-4 mx-auto my-5">
               <div className="card">
                 <div className="card-body">
-                  <h1 id="pick-card">
-
-                  </h1>
+                  <div>
+                  <img src={image} className="card-img" alt="" />
+                  </div>
+                  <h1>{data}</h1>
+                  <p>{title}</p>
                 </div>
               </div>
             </div>
